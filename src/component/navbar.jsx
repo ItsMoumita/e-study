5cf55b40dbbc23e84c2af1127a7eb380d0807965
Fatema-Tuj-Zahura Moumita <fatihamoumita98@gmail.com>
2025-08-10 12:39:02 +0600

    modified footer

--- src/component/navbar.jsx
+++ src/component/navbar.jsx
@@ -1,5 +1,5 @@
 import React, { useContext, useState } from "react";
-import { NavLink } from "react-router-dom";
+import { NavLink } from "react-router";
 import { FaMoon, FaBars, FaChevronDown } from "react-icons/fa";
 import { MdLightMode } from "react-icons/md";
 import ThemeToggle from "./ThemeToggle";
